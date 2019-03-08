822f80934e878f2480c84d2be3c7d2234425e3bd
Michael Gamatero <mgamatero@yahoo.com>
2019-03-08 12:06:43 -0800

    App finished. Geolocation and month determine screen to show.  Ternary, seasonConfig, loading screen. .defaultProps used

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,50 @@
+import './SeasonDisplay.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const SeasonDisplay = () => {
-    return <div>Hi There Boy</div>
+//You can place logic out here too
+const getSeason = (lat,month) =>{
+    if (month>2 && month<9){
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+// const seasonConfig = { my try here
+//     icon: getSeason() === 'winter' ? 'snowflake' : 'sun',
+//     text: getSeason() === 'winter' ? 'Brrrr, it is chilly' : 'Time for the beach!!!'
+// }
+
+const seasonConfig = {
+    summer:{
+        icon:'sun',
+        text:'Time for the beach!!!'
+    },
+    winter:{
+        icon:'snowflake',
+        text:'Brrrr, it is chilly!!!'
+    }
+}
+
+
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.latitude, new Date().getMonth());
+    //destructuring...if not you say seasonConfig[season].text....ugly
+    const {text,icon} = seasonConfig[season];
+
+
+    console.log(season,props.latitue)
+
+    return (
+        <div className = {`season-display ${season}`}>
+        <i className={`${icon} icon massive icon-left`}/>
+        <h1>{text}</h1>
+        <i className={`${icon} icon massive icon-right`}/>
+        
+        </div>
+    )
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
+
+    //JUST DO IT!!!!  Add props.  JUST DO IT!!!!
+    constructor(props) {
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            errorMessage: ''
+        };     
+    };
+
+    // This works too --  Better to initialize
+    // state = {
+    //     latitude: null,
+    //     longitude: null,
+    //     errorMessage: ''
+    // };     
     
-    //JUST DO IT!!!!
-    constructor() {
-        super()
-        this.state ={
-            latitude : 0
-        }
-    }
-
-    // window.navigator.geolocation.getCurrentPosition((position) => console.log(position),
-    //     err => console.log(err)
-    //     );
-
-    render(){
-
+    
+    componentDidMount() {
+        // console.log('CompenentDidMount')
         //geolocation API
-        window.navigator.geolocation.getCurrentPosition((position) => 
-        {console.log(position)
-        this.setState({latitude:position.coords.latitude})
-        },
-        err => console.log(err)
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+            console.log(position)
+            this.setState({ latitude: position.coords.latitude })
+            this.setState({ longitude: position.coords.longitude })
+            },
+            err => {
+                console.log(err)
+                this.setState({ errorMessage: err.message });
+            }
         );
+    }
+    // }
+    // componentDidUpdate(){
+    //     console.log('CompenentDidUpdate - Rerendered')
+    // }
+    render() {
+
+        // Conditional rendering
+        if (this.state.errorMessage && !this.state.latitude && !this.state.longitude) {
+            return (
+                <div>Error: {this.state.errorMessage}</div>
+            )
+        }
+        if (!this.state.latitude && !this.state.longitude && !this.state.err) {
+            return (
+                <Spinner message="Loading...Default not used..."/>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <SeasonDisplay latitude={this.state.latitude}/>
+                    
+                </div>
+            )
+        }
 
 
 
-        return <div>Latitude: {this.state.latitude}</div>
+
+
+
+
+
+
     }
 };
 
